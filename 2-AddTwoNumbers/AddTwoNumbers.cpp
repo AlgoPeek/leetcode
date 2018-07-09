369ceb0e05ae1a1039fbd8f6d877164416b7ce42
Diego Lee <ksc_liyong@163.com>
2018-07-09 23:47:46 +0800

    format text

--- 2-AddTwoNumbers/AddTwoNumbers.cpp
+++ 2-AddTwoNumbers/AddTwoNumbers.cpp
@@ -19,87 +19,87 @@
 
 struct ListNode
 {
-	int val;
-	ListNode* next;
-	ListNode(int x): val(x), next(NULL) {}
+    int val;
+    ListNode* next;
+    ListNode(int x): val(x), next(NULL) {}
 };
 
 class Solution
 {
-public:
-	ListNode* addTwoNumbers(ListNode* L1, ListNode* L2)
-    {
-		ListNode* L3 = new ListNode(0);
-		ListNode* p1 = L1;
-		ListNode* p2 = L2;
-		ListNode* p3 = L3;
-		int carry = 0;
-		while (p1 || p2)
+    public:
+        ListNode* addTwoNumbers(ListNode* L1, ListNode* L2)
         {
-			if (p1)
+            ListNode* L3 = new ListNode(0);
+            ListNode* p1 = L1;
+            ListNode* p2 = L2;
+            ListNode* p3 = L3;
+            int carry = 0;
+            while (p1 || p2)
             {
-				carry += p1->val;
-				p1 = p1->next;
-			}
-			if (p2)
-            {
-				carry += p2->val;
-				p2 = p2->next;
-			}
-			p3->next = new ListNode(carry % 10);
-			p3 = p3->next;
-			carry /= 10;
-		}
+                if (p1)
+                {
+                    carry += p1->val;
+                    p1 = p1->next;
+                }
+                if (p2)
+                {
+                    carry += p2->val;
+                    p2 = p2->next;
+                }
+                p3->next = new ListNode(carry % 10);
+                p3 = p3->next;
+                carry /= 10;
+            }
 
-		if (carry > 0)
-        {
-			p3->next = new ListNode(carry);
-		}
-		return L3->next;
-	}
+            if (carry > 0)
+            {
+                p3->next = new ListNode(carry);
+            }
+            return L3->next;
+        }
 };
 
 void CreateList(ListNode** root, int* vals, int pos, int len)
 {
-	if (pos < len)
+    if (pos < len)
     {
-		ListNode*& _root = *root;
-		_root = new ListNode(vals[pos]);
-		CreateList(&_root->next, vals, pos + 1, len);
-	}
+        ListNode*& _root = *root;
+        _root = new ListNode(vals[pos]);
+        CreateList(&_root->next, vals, pos + 1, len);
+    }
 }
 
 void PrintList(ListNode* root)
 {
     ListNode* next = root;
-	while (next)
+    while (next)
     {
-		std::cout << root->val << " ";
-		next = next->next;
-	}
-	std::cout << std::endl;
+        std::cout << root->val << " ";
+        next = next->next;
+    }
+    std::cout << std::endl;
 }
 
 void testAddTwoNumbers()
 {
-	int a1[] = {1, 8};
-	int a2[] = {0};
-	ListNode* L1 = NULL;
-	ListNode* L2 = NULL;
-	CreateList(&L1, a1, 0, sizeof(a1) / sizeof(a1[0]));
-	CreateList(&L2, a2, 0, sizeof(a2) / sizeof(a2[0]));
-	PrintList(L1);
-	PrintList(L2);
+    int a1[] = {1, 8};
+    int a2[] = {0};
+    ListNode* L1 = NULL;
+    ListNode* L2 = NULL;
+    CreateList(&L1, a1, 0, sizeof(a1) / sizeof(a1[0]));
+    CreateList(&L2, a2, 0, sizeof(a2) / sizeof(a2[0]));
+    PrintList(L1);
+    PrintList(L2);
 
-	ListNode* L3 = Solution().addTwoNumbers(L1, L2);
-	PrintList(L3);
-	assert(L3->val == 1);
-	assert(L3->next->val == 8);
+    ListNode* L3 = Solution().addTwoNumbers(L1, L2);
+    PrintList(L3);
+    assert(L3->val == 1);
+    assert(L3->next->val == 8);
 }
 
 int main()
 {
-	testAddTwoNumbers();
-	return 0;
+    testAddTwoNumbers();
+    return 0;
 }
 
