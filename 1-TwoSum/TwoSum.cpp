c349b1a2eb859369090f76af832a69e1c95bc0f1
Diego Lee <ksc_liyong@163.com>
2018-07-09 23:33:12 +0800

    format code

--- 1-TwoSum/TwoSum.cpp
+++ 1-TwoSum/TwoSum.cpp
@@ -21,42 +21,42 @@
 class Solution
 {
 public:
-	std::vector<int> twoSum(std::vector<int>& nums, int target)
+    std::vector<int> twoSum(std::vector<int>& nums, int target)
     {
-		std::vector<int> result;
-		std::map<int, int> viMap;
-		for (size_t i = 0; i < nums.size(); ++i)
+        std::vector<int> result;
+        std::map<int, int> viMap;
+        for (size_t i = 0; i < nums.size(); ++i)
         {
-			int val = nums[i];
-			std::map<int, int>::iterator it = viMap.find(val);
-			if (it != viMap.end())
+            int val = nums[i];
+            std::map<int, int>::iterator it = viMap.find(val);
+            if (it != viMap.end())
             {
-				result.push_back(it->second + 1);
-				result.push_back(i + 1);
-				break;
-			}
-			else
+                result.push_back(it->second + 1);
+                result.push_back(i + 1);
+                break;
+            }
+            else
             {
-				viMap.insert(std::make_pair(target - val, i));
-			}
-		}
-		return result;
-	}
+                viMap.insert(std::make_pair(target - val, i));
+            }
+        }
+        return result;
+    }
 };
 
 void testTwoSumCase(int* A, int n, int target)
 {
-	std::vector<int> nums(A, A + n);
+    std::vector<int> nums(A, A + n);
     std::vector<int> result = Solution().twoSum(nums, target);
-	assert(result.size() == 1);
-	assert(result[0] == 1);
-	assert(result[1] == 2);
+    assert(result.size() == 1);
+    assert(result[0] == 1);
+    assert(result[1] == 2);
 }
 
 int main()
 {
     int A[] = {2, 7, 11, 15};
     int target = 9;
-	testTwoSumCase(A, sizeof(A) / sizeof(int), target);
-	return 0;
+    testTwoSumCase(A, sizeof(A) / sizeof(int), target);
+    return 0;
 }
