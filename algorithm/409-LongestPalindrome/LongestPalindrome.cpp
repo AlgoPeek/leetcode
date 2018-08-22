6bcf92ec0766d725eb78b105621915a7d261b94f
Diego Lee <ksc_liyong@163.com>
2018-08-23 00:00:32 +0800

    add unittest for 409

--- algorithm/409-LongestPalindrome/LongestPalindrome.cpp
+++ algorithm/409-LongestPalindrome/LongestPalindrome.cpp
@@ -2,6 +2,24 @@
 // Author: Diego Lee
 // Date: 2018-08-22
 //
+// Description:
+// Given a string which consists of lowercase or uppercase letters, find the length of the longest palindromes that can be built with those letters.
+//
+// This is case sensitive, for example "Aa" is not considered a palindrome here.
+//
+// Note:
+// Assume the length of given string will not exceed 1,010.
+//
+// Example:
+//
+// Input:
+// "abccccdd"
+//
+// Output:
+// 7
+//
+// Explanation:
+// One longest palindrome that can be built is "dccaccd", whose length is 7.
 
 #include <iostream>
 #include <string>
@@ -53,5 +71,6 @@ void testLongestPalindrome()
 
 int main()
 {
+    testLongestPalindrome();
     return 0;
 }
