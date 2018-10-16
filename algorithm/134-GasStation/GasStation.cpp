dff84b8fec02f6ed91d4714cec36ef11b087538f
Diego Lee <ksc_liyong@163.com>
2018-10-16 20:16:42 +0800

    add Gas Station question implemented by cpp

---
+++ algorithm/134-GasStation/GasStation.cpp
@@ -0,0 +1,92 @@
+// Source: https://leetcode.com/problems/gas-station/description/
+// Author: Diego Lee
+// Date: 2-10-10-16
+//
+// Description:
+// There are N gas stations along a circular route, where the amount of gas at station i is gas[i].
+//
+// You have a car with an unlimited gas tank and it costs cost[i] of gas to travel from station i to its next station (i+1). You begin the journey with an empty tank at one of the gas stations.
+//
+// Return the starting gas station's index if you can travel around the circuit once in the clockwise direction, otherwise return -1.
+//
+// Note:
+//
+// If there exists a solution, it is guaranteed to be unique.
+// Both input arrays are non-empty and have the same length.
+// Each element in the input arrays is a non-negative integer.
+// Example 1:
+//
+// Input:
+// gas  = [1,2,3,4,5]
+// cost = [3,4,5,1,2]
+//
+// Output: 3
+//
+// Explanation:
+// Start at station 3 (index 3) and fill up with 4 unit of gas. Your tank = 0 + 4 = 4
+// Travel to station 4. Your tank = 4 - 1 + 5 = 8
+// Travel to station 0. Your tank = 8 - 2 + 1 = 7
+// Travel to station 1. Your tank = 7 - 3 + 2 = 6
+// Travel to station 2. Your tank = 6 - 4 + 3 = 5
+// Travel to station 3. The cost is 5. Your gas is just enough to travel back to station 3.
+// Therefore, return 3 as the starting index.
+// Example 2:
+//
+// Input:
+// gas  = [2,3,4]
+// cost = [3,4,3]
+//
+// Output: -1
+//
+// Explanation:
+// You can't start at station 0 or 1, as there is not enough gas to travel to the next station.
+// Let's start at station 2 and fill up with 4 unit of gas. Your tank = 0 + 4 = 4
+// Travel to station 0. Your tank = 4 - 3 + 2 = 3
+// Travel to station 1. Your tank = 3 - 3 + 3 = 3
+// You cannot travel back to station 2, as it requires 4 unit of gas but you only have 3.
+// Therefore, you can't travel around the circuit once no matter where you start.
+
+#include <iostream>
+#include <vector>
+#include <cassert>
+
+class Solution {
+    public:
+        int canCompleteCircuit(std::vector<int>& gas, std::vector<int>& cost) {
+            if (gas.size() != cost .size()) {
+                return -1;
+            }
+
+            for (int i = 0, len = gas.size(); i < len; ++i) {
+                int tank = gas[i];
+                int start = i;
+                int count = len;
+                while (count-- > 0) {
+                    int costindex = start;
+                    start = (start + 1) % len;
+                    if (tank < cost[costindex]) {
+                        tank = -1;
+                        break;
+                    }
+                    tank += gas[start] - cost[costindex];
+                }
+                if (tank - gas[start] >= 0) {
+                    return i;
+                }
+            }
+            return -1;
+        }
+};
+
+void testCanCompleteCircuit() {
+    std::vector<int> gas = {1,2,3,4,5,5,70};
+    std::vector<int> cost = {2,3,4,3,9,6,2};
+    Solution s;
+    int result = s.canCompleteCircuit(gas, cost);
+    assert(result == 6);
+}
+
+int main() {
+    testCanCompleteCircuit();
+    return 0;
+}
